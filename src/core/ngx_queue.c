4bd1d147dd92d8c93ad89a53236c7309484c5940
gxpisme <gxpisme@gmail.com>
2019-01-16 11:28:06 +0800

    ngx queue

--- src/core/ngx_queue.c
+++ src/core/ngx_queue.c
@@ -12,6 +12,8 @@
 /*
  * find the middle queue element if the queue has odd number of elements
  * or the first element of the queue's second part otherwise
+ *
+ * 找中间元素
  */
 
 ngx_queue_t *
@@ -19,25 +21,33 @@ ngx_queue_middle(ngx_queue_t *queue)
 {
     ngx_queue_t  *middle, *next;
 
+    //链表的头元素
     middle = ngx_queue_head(queue);
 
+    //链表的头元素与尾元素相等，则就一个元素，返回该元素即可
     if (middle == ngx_queue_last(queue)) {
         return middle;
     }
 
+    //这里也是头元素
     next = ngx_queue_head(queue);
 
+    //开始循环
+    //1.采用了每循环一次，middle向后移一个指针
+    //2.每循环一次，next向后移动两个指针
+    //3.middle走一步，next走两步，当next走到尾元素，则正好输出middle，完美
     for ( ;; ) {
+        //从头元素开始，middle依次向后，每循环一次，向后移一个指针
         middle = ngx_queue_next(middle);
 
+        //也是从头元素开始，next，每循环一次，向后移动两个指针
+        //next每移动一次之后，要判读next与最后一个元素是否相等
         next = ngx_queue_next(next);
-
         if (next == ngx_queue_last(queue)) {
             return middle;
         }
 
         next = ngx_queue_next(next);
-
         if (next == ngx_queue_last(queue)) {
             return middle;
         }
@@ -47,26 +57,37 @@ ngx_queue_middle(ngx_queue_t *queue)
 
 /* the stable insertion sort */
 
+/**
+ * 无须的双向链表，排序成有序的双向链表 很好玩，很精妙的逻辑
+ */
 void
 ngx_queue_sort(ngx_queue_t *queue,
     ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *))
 {
     ngx_queue_t  *q, *prev, *next;
 
+    //链表的头元素
     q = ngx_queue_head(queue);
 
+    //头元素等于尾元素
     if (q == ngx_queue_last(queue)) {
         return;
     }
 
+    //从头元素向后循环
     for (q = ngx_queue_next(q); q != ngx_queue_sentinel(queue); q = next) {
 
+        //向前遍历
         prev = ngx_queue_prev(q);
+        //向后遍历
         next = ngx_queue_next(q);
 
+        //移除q元素
         ngx_queue_remove(q);
 
+        //该循环保证了，q大于prev的最大值，一定q放在了最大值prev的后面
         do {
+            //当q大于prev时，退出循环
             if (cmp(prev, q) <= 0) {
                 break;
             }
@@ -75,6 +96,7 @@ ngx_queue_sort(ngx_queue_t *queue,
 
         } while (prev != ngx_queue_sentinel(queue));
 
+        //将q元素插入在prev的后面，保证q元素大于prev
         ngx_queue_insert_after(prev, q);
     }
 }
--- src/core/ngx_queue.h
+++ src/core/ngx_queue.h
@@ -13,23 +13,42 @@
 #define _NGX_QUEUE_H_INCLUDED_
 
 
+/**
+ * 队列结构，其实质是具有头节点的双向循环链表
+ */
 typedef struct ngx_queue_s  ngx_queue_t;
 
+/**
+ * 队列中每个节点结构，只有两个指针，并没有数据区
+ */
 struct ngx_queue_s {
     ngx_queue_t  *prev;
     ngx_queue_t  *next;
 };
 
 
+/**
+ * q为链表容器结构体ngx_queue_t的指针
+ * 链表容器q初始化，会设置为空的链表
+ */
 #define ngx_queue_init(q)                                                     \
     (q)->prev = q;                                                            \
     (q)->next = q
 
 
+/**
+ * 检测链表容器是否为空
+ */
 #define ngx_queue_empty(h)                                                    \
     (h == (h)->prev)
 
 
+/**
+ * 将元素x，插入h链表的头部
+ *
+ * 图解
+ * http://cdn.xpisme.com/201901151909_496.png
+ */
 #define ngx_queue_insert_head(h, x)                                           \
     (x)->next = (h)->next;                                                    \
     (x)->next->prev = x;                                                      \
@@ -40,6 +59,12 @@ struct ngx_queue_s {
 #define ngx_queue_insert_after   ngx_queue_insert_head
 
 
+/**
+ * 将元素x，插入h链表的尾部
+ *
+ * 图解
+ * http://cdn.xpisme.com/201901152024_507.png
+ */
 #define ngx_queue_insert_tail(h, x)                                           \
     (x)->prev = (h)->prev;                                                    \
     (x)->prev->next = x;                                                      \
@@ -47,10 +72,15 @@ struct ngx_queue_s {
     (h)->prev = x
 
 
+/**
+ * 链表的头元素
+ */
 #define ngx_queue_head(h)                                                     \
     (h)->next
 
-
+/**
+ * 链表的尾元素
+ */
 #define ngx_queue_last(h)                                                     \
     (h)->prev
 
@@ -58,17 +88,24 @@ struct ngx_queue_s {
 #define ngx_queue_sentinel(h)                                                 \
     (h)
 
-
+/**
+ * 链表的后一个元素
+ */
 #define ngx_queue_next(q)                                                     \
     (q)->next
 
-
+/**
+ * 链表的前一个元素
+ */
 #define ngx_queue_prev(q)                                                     \
     (q)->prev
 
 
 #if (NGX_DEBUG)
 
+/**
+ * 在链表中移除x元素
+ */
 #define ngx_queue_remove(x)                                                   \
     (x)->next->prev = (x)->prev;                                              \
     (x)->prev->next = (x)->next;                                              \
@@ -84,6 +121,14 @@ struct ngx_queue_s {
 #endif
 
 
+/**
+ * h为链表容器结构体ngx_queue_t的指针 该函数用于拆分链表
+ * h是链表容器，而q是链表h中的一个元素。
+ * 将链表h以元素q为界拆分成两个链表 h 和 n
+ *
+ * 图解
+ * http://cdn.xpisme.com/201901152056_612.png
+ */
 #define ngx_queue_split(h, q, n)                                              \
     (n)->prev = (h)->prev;                                                    \
     (n)->prev->next = n;                                                      \
@@ -92,7 +137,13 @@ struct ngx_queue_s {
     (h)->prev->next = h;                                                      \
     (q)->prev = n;
 
-
+/**
+ * h为链表容器结构体ngx_queue_t的指针，n为另一个链表容器结构体ngx_queue_t的指针
+ * 合并链表，将n链表添加到h链表的末尾
+ *
+ * 图解
+ * http://cdn.xpisme.com/201901161012_123.png
+ */
 #define ngx_queue_add(h, n)                                                   \
     (h)->prev->next = (n)->next;                                              \
     (n)->next->prev = (h)->prev;                                              \
